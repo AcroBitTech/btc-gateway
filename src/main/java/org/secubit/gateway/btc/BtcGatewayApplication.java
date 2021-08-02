17bab94934be0695480618f3785f64501ec890b7
Ming Zhao <ming@acrosafe.io>
2021-08-02 13:52:27 -0400

    initialized the project

---
+++ src/main/java/org/secubit/gateway/btc/BtcGatewayApplication.java
@@ -0,0 +1,15 @@
+package org.secubit.gateway.btc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtcGatewayApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(BtcGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/secubit/gateway/btc/BtcGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.secubit.gateway.btc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtcGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
